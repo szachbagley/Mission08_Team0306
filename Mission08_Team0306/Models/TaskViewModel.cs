1543b0deeee5469bd1d7048c5972977f059e129c
Zach Bagley <szachbagley@gmail.com>
2024-02-29 20:44:16 -0700

    trying to fix git

---
+++ Mission08_Team0306/Models/TaskViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Mission08_Team0306.Models
+{
+    public class TaskViewModel
+    {
+        [Key]
+        [Required]
+
+        public int TaskId { get; set; }
+
+        [ForeignKey("QuadrantId")]
+        [Required]
+        public int QuadrantId { get; set; }
+        public Quadrant Quadrants { get; set; }
+
+
+        [ForeignKey("CategoryId")]
+        [Required]
+        public int CategoryId { get; set; }
+        public Categories Category { get; set; }
+
+        [Required]
+        public string TaskDescription { get; set; }
+
+        
+
+        public string? DueDate { get; set; }
+
+        public bool Completed { get; set; }
+    }
+}
